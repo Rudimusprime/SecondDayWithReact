9418ff8b64ad73164caa07d1e31e288581b9d7df
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-07 19:33:32 +0200

    first example finished

--- FirstExample/scripts/App.js
+++ FirstExample/scripts/App.js
@@ -3,18 +3,26 @@ class App extends React.Component {
         value: "",
     }
 
-    handleInputChange = (e) => {
-        console.log(e.target.value);
+    handleChange = (e) => {
+        console.log(`Zawartość w evencie ${e.target.value}`);
+        console.log(`Zawartość value przed setState:  ${this.state.value}`);
         this.setState({
             value: e.target.value
+        })
+        console.log(`Zawartość value po setState: ${this.state.value}`);
+    }
+    handleClick = () => {
+        this.setState({
+            value: "",
         })
     }
 
     render() {
+        console.log(`Zawartość value w trakcie metory redner: ${this.state.value}`);
         return (
             <React.Fragment>
-                <input placeholder="wpisz..." onChange={this.handleInputChange} type="text"/>
-                <button>Reset</button>
+                <input value={this.state.value} placeholder="wpisz..." onChange={this.handleChange} type="text"/>
+                <button onClick={this.handleClick}>Reset</button>
                 <h1 className="title">{this.state.value.toUpperCase()}</h1>
             </React.Fragment>
         )
